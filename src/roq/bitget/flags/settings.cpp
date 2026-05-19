b3458e7c865e30e1c0cd0453de04d439765f2255
thraneh <thraneh@gmail.com>
2026-05-19 15:28:34 +0000

    move gateway settings to flags

--- src/roq/bitget/flags/settings.cpp
+++ src/roq/bitget/flags/settings.cpp
@@ -1,6 +1,6 @@
 /* Copyright (c) 2017-2026, Hans Erik Thrane */
 
-#include "roq/bitget/settings.hpp"
+#include "roq/bitget/flags/settings.hpp"
 
 #include "roq/logging.hpp"
 
@@ -8,6 +8,7 @@ using namespace std::literals;
 
 namespace roq {
 namespace bitget {
+namespace flags {
 
 Settings::Settings(args::Parser const &args)
     : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER}, flags::Flags{flags::Flags::create()}, misc{flags::Misc::create()},
@@ -15,5 +16,6 @@ Settings::Settings(args::Parser const &args)
   log::info("settings={}"sv, *this);
 }
 
+}  // namespace flags
 }  // namespace bitget
 }  // namespace roq
--- src/roq/bitget/settings.hpp
+++ src/roq/bitget/settings.hpp
@@ -2,45 +2,12 @@
 
 #pragma once
 
-#include <fmt/format.h>
-
-#include "roq/server/flags/settings.hpp"
-
-#include "roq/bitget/flags/flags.hpp"
-#include "roq/bitget/flags/misc.hpp"
-#include "roq/bitget/flags/rest.hpp"
-#include "roq/bitget/flags/ws.hpp"
+#include "roq/bitget/flags/settings.hpp"
 
 namespace roq {
 namespace bitget {
 
-struct Settings final : public server::flags::Settings, public flags::Flags {
-  explicit Settings(args::Parser const &);
-
-  flags::Misc misc;
-  flags::REST rest;
-  flags::WS ws;
-};
+using Settings = flags::Settings;
 
 }  // namespace bitget
 }  // namespace roq
-
-template <>
-struct fmt::formatter<roq::bitget::Settings> {
-  constexpr auto parse(format_parse_context &context) { return std::begin(context); }
-  auto format(roq::bitget::Settings const &value, format_context &context) const {
-    using namespace std::literals;
-    return fmt::format_to(
-        context.out(),
-        R"({{)"
-        R"(misc={}, )"
-        R"(rest={}, )"
-        R"(ws={}, )"
-        R"(server={})"
-        R"(}})"sv,
-        value.misc,
-        value.rest,
-        value.ws,
-        static_cast<roq::server::Settings const &>(value));
-  }
-};
