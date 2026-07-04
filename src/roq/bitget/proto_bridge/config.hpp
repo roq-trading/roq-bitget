c219f179c2bee28f13640bfd1d6a9d1528f5c095
thraneh <thraneh@gmail.com>
2026-07-04 03:18:56 +0000

    preparing roq-*-proto-bridge

---
+++ src/roq/bitget/proto_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/bitget/gateway/config.hpp"
+
+namespace roq {
+namespace bitget {
+namespace proto_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace proto_bridge
+}  // namespace bitget
+}  // namespace roq
